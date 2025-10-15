3fb1146c0b5fba67797582fc48f4601ac6960452
Tarun DIdwaniya <89730122+tarun9929@users.noreply.github.com>
2025-10-15 08:42:40 +0000

    feat: add CORS middleware and configuration for handling cross-origin requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import { cors_origin } from './config/dotenv.config.js';
 
 const app = express();
 
 // middlewares
+
+app.use(cors({
+    origin: cors_origin
+}));
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true ,limit: "16kb"}));
@@ -10,5 +16,24 @@ app.use(express.urlencoded({ extended: true ,limit: "16kb"}));
 app.use(express.static("public"));
 
 // routes
+
+
+// test routes 
+app.get("/api/v1/test", (req, res) => {
+    const users = [
+        {
+            name: "Tarun Didwaniya",
+            email: "tarundidwaniya4@gmail.com",
+            age: 23
+        },
+        {
+            name: "John Doe",
+            email: "johndoe@gmail.com",
+            age: 30
+        }
+    ];
+
+    res.status(200).json(users)  
+})
 
 export default app;
--- server/src/config/dotenv.config.js
+++ server/src/config/dotenv.config.js
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const port = process.env.PORT || 5000;
 export const mongodb_url = process.env.MONGODB_URL;
+export const cors_origin = process.env.CORS_ORIGIN
