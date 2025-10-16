505a7e696401145b94355615c57903565a6e054c
Tarun DIdwaniya <89730122+tarun9929@users.noreply.github.com>
2025-10-16 07:54:16 +0000

    feat: add LandingPage component and update routing in main.jsx

---
+++ client/src/Components/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function LandingPage() {
+    return (
+        <>
+            <div>
+                <h1>Landing Page</h1>
+                <p>Welcome to the landing page!</p>
+                <p>Username: user1201210</p>
+                <p>Userid: aeWKak@4554a</p>
+                <p>Email: demoemail@gmail.com</p>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage;
