5e5d19c483615959395ce9db67b0e24e21eadf23
Tarun DIdwaniya <89730122+tarun9929@users.noreply.github.com>
2025-10-17 06:53:48 +0000

    feat: add User model with authentication methods and dotenv configuration

--- server/src/config/dotenv.config.js
+++ server/src/config/dotenv.config.js
@@ -4,4 +4,8 @@ dotenv.config();
 
 export const port = process.env.PORT || 5000;
 export const mongodb_url = process.env.MONGODB_URL;
-export const cors_origin = process.env.CORS_ORIGIN
+export const cors_origin = process.env.CORS_ORIGIN;
+export const secret_access_token = process.env.SECRET_ACCESS_TOKEN;
+export const access_duration = process.env.ACCESS_DURATION;
+export const secret_refresh_token = process.env.SECRET_REFRESH_TOKEN;
+export const refresh_duration = process.env.REFRESH_DURATION;
